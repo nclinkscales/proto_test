745004c8a94f9608d1f070e1923e2013c570a5da
NickClinkscales <nclinkscales@lostoutpoststudios.com>
2020-08-18 18:17:17 -0400

    Tile

---
+++ Assets/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2 pos;
+    public double height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetUnit()
+    {
+
+    }
+
+    public void RemoveUnit()
+    {
+
+    }
+}
