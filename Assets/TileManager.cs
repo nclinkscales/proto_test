16c396ea9caf4d4b0ccd2dd705658b68b522af1f
NickClinkscales <nclinkscales@lostoutpoststudios.com>
2020-08-18 18:40:19 -0400

    TileManager

---
+++ Assets/TileManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    Tile[,] tiles = new Tile[0, 0];
+
+    public Tile GetTile(Vector2 pos)
+    {
+        return tiles[(int)pos.x, (int)pos.y];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
